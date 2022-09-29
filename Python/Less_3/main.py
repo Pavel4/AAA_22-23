10b51f7384deb358a4e82de063791b44163cc891
Pavel <pavel2195@gmail.com>
2022-09-29 15:49:37 +0300

    Without pep8 and docstrings

--- Python/Less_3/main.py
+++ Python/Less_3/main.py
@@ -2,6 +2,11 @@ import json
 import keyword
 
 
+class ColorizeMixin:
+    def __repr__(self, color_code: int):
+        return f'\033[1;{color_code};40m'
+
+
 class JSONToObject:
 
     def __init__(self, mapping: [dict, str]) -> None:
@@ -17,11 +22,23 @@ class JSONToObject:
             self.__init__(json.loads(mapping))
 
 
-class Advert(JSONToObject):
+class Advert(ColorizeMixin, JSONToObject):
+    repr_color_code_green = 32
+
     def __init__(self, mapping: [dict, str]) -> None:
         JSONToObject.__init__(self, mapping)
+        if not hasattr(self, 'title'):
+            self._title = None
         if not hasattr(self, 'price'):
             self._price = 0
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        self._title = value
 
     @property
     def price(self):
@@ -33,14 +50,26 @@ class Advert(JSONToObject):
             raise ValueError('must be >= 0')
         self._price = value
 
+    def __repr__(self):
+        return super().__repr__(self.repr_color_code_green) + f'{self.title} | {self.price} ₽'
+
 
 if __name__ == "__main__":
-    json_1 = {
+    iphone_json = {
         "title": "iPhone X",
         "price": 100,
         "location": {
             "address": "город Самара, улица Мориса Тореза, 50",
             "metro_stations": ["Спортивная", "Гагаринская"]
+        }
+    }
+
+    corgi_json = {
+        "title": "Вельш-корги",
+        "price": 1000,
+        "class": "dogs",
+        "location": {
+            "address": "сельское поселение Ельдигинское, поселок санатория Тишково, 25"
         }
     }
 
@@ -53,8 +82,12 @@ if __name__ == "__main__":
     }
     }"""
 
-    lesson = json.loads(lesson_str)
+    lesson_ad = json.loads(lesson_str)
 
-    lesson_ad = Advert(json_1)
+    lesson = Advert(lesson_ad)
+    corgi = Advert(corgi_json)
+    iphone = Advert(iphone_json)
 
-    print(lesson_ad.price)
+    print(lesson.location.address)
+    print(iphone.location.address)
+    print(iphone)
