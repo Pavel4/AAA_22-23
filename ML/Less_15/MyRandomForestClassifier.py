ae3d2cc8a38e6437a0f1b7602dfbf6df3344b7b4
Pavel <pavel2195@gmail.com>
2022-12-25 20:41:41 +0300

    Add MyRandomForestClassifier.py

---
+++ ML/Less_15/MyRandomForestClassifier.py
@@ -0,0 +1,186 @@
+import numpy as np
+
+
+class MyDecisionTreeClassifier:
+
+    def __init__(self, max_depth=None, max_features=None, min_leaf_samples=None, classes=None):
+        self.max_depth = max_depth
+        self.max_features = max_features
+        self.min_leaf_samples = min_leaf_samples
+        self._node = {
+            'left': None,
+            'right': None,
+            'feature': None,
+            'threshold': None,
+            'depth': 0,
+            'classes_proba': None
+        }
+        self.tree = None  # словарь в котором будет храниться построенное дерево
+        self.classes = classes  # список меток классов
+
+    def fit(self, X, y):
+
+        if self.classes is None:
+            self.classes = np.unique(y)
+
+        self.tree = {'root': self._node.copy()}  # создаём первую узел в дереве
+        self._build_tree(self.tree['root'], X, y)  # запускаем рекурсивную функцию для построения дерева
+        return self
+
+    def predict_proba(self, X):
+        proba_preds = []
+        for x in X:
+            preds_for_x = self._get_predict(self.tree['root'],
+                                            x)  # рекурсивно ищем лист в дереве соответствующий объекту
+            proba_preds.append(preds_for_x)
+        return np.array(proba_preds)
+
+    def predict(self, X):
+        proba_preds = self.predict_proba(X)
+        preds = proba_preds.argmax(axis=1).reshape(-1, 1)
+        return preds
+
+    def get_best_split(self, X, y):
+
+        best_Q = -1
+        best_j = 0
+        best_t = 0
+        best_left_ids = []
+        best_right_ids = []
+
+        n, m = X.shape[0], X.shape[1]
+
+        for j in range(m):
+            X_set = np.unique(X[:, j])
+            thresholds = [(X_set[i] + X_set[i + 1]) / 2 for i in range(len(X_set) - 1)]
+            for threshold in thresholds:
+                left_ids = [X[:, j] <= threshold]
+                right_ids = [X[:, j] > threshold]
+
+                left_tree = y[tuple(left_ids)]
+                right_tree = y[tuple(right_ids)]
+                Q = self.calc_Q(y, left_tree, right_tree)
+                if Q > best_Q:
+                    best_Q = Q
+                    best_j = j
+                    best_t = threshold
+                    best_left_ids = left_ids
+                    best_right_ids = right_ids
+
+        return best_j, best_t, best_left_ids, best_right_ids
+
+    def calc_Q(self, y, y_left, y_right):
+        return self.gini(y) - (y_left.shape[0] / y.shape[0] * self.gini(y_left)
+                               + y_right.shape[0] / y.shape[0] * self.gini(y_right))
+
+    def gini(self, y):
+        y_len = y.shape[0]
+        return 1 - sum([(np.sum(y == y_) / y_len) ** 2 for y_ in np.unique(y)])
+
+    def _build_tree(self, curr_node, X, y):
+
+        if curr_node['depth'] == self.max_depth:  # выход из рекурсии если построили до максимальной глубины
+            curr_node['classes_proba'] = {c: (y == c).mean() for c in
+                                          self.classes}  # сохраняем предсказания листьев дерева перед выходом из рекурсии
+            return
+
+        if len(np.unique(y)) == 1:  # выход из рекурсии значения если "y" одинковы для все объектов
+            curr_node['classes_proba'] = {c: (y == c).mean() for c in self.classes}
+            return
+
+        j, t, left_ids, right_ids = self.get_best_split(X, y)  # нахождение лучшего разбиения
+
+        curr_node['feature'] = j  # признак по которому производится разбиение в текущем узле
+        curr_node['threshold'] = t  # порог по которому производится разбиение в текущем узле
+
+        left = self._node.copy()  # создаём узел для левого поддерева
+        right = self._node.copy()  # создаём узел для правого поддерева
+
+        left['depth'] = curr_node['depth'] + 1  # увеличиваем значение глубины в узлах поддеревьев
+        right['depth'] = curr_node['depth'] + 1
+
+        curr_node['left'] = left
+        curr_node['right'] = right
+
+        self._build_tree(left, X[left_ids], y[left_ids])  # продолжаем построение дерева
+        self._build_tree(right, X[right_ids], y[right_ids])
+
+    def _get_predict(self, node, x):
+        if node['threshold'] is None:  # если в узле нет порога, значит это лист, выходим из рекурсии
+            return [node['classes_proba'][c] for c in self.classes]
+
+        if x[node['feature']] <= node[
+            'threshold']:  # уходим в правое или левое поддерево в зависимости от порога и признака
+            return self._get_predict(node['left'], x)
+        else:
+            return self._get_predict(node['right'], x)
+
+
+class MyRandomForestClassifier:
+
+    def __init__(self, max_features, n_estimators=100, max_depth=None):
+        self.n_classes = None
+        self.n_estimators = n_estimators
+        self.max_features = max_features
+        self.max_depth = max_depth
+        self.forest = []
+        self.classes = None
+
+    def fit(self, X, y):
+        self.forest = []
+        self.classes = np.unique(y)
+        self.n_classes = len(self.classes)
+        indexes = np.arange(X.shape[0])
+        for _ in range(self.n_estimators):
+            indexes_bootstrap = np.random.choice(indexes, size=X.shape[0], replace=True)
+            X_bootstrap = X[indexes_bootstrap]
+            y_bootstrap = y[indexes_bootstrap]
+            dt = MyDecisionTreeClassifier(max_depth=None, max_features=self.max_features, classes=self.classes)
+            dt.fit(X_bootstrap, y_bootstrap)
+            self.forest.append(dt)
+
+        return self
+
+    def predict(self, X):
+        preds = self.predict_proba(X)
+        return np.argmax(preds, axis=1)
+
+    def predict_proba(self, X):
+        preds_matrix = np.zeros(shape=(X.shape[0], self.n_classes, self.n_estimators))
+
+        for i, dt in enumerate(self.forest):
+            preds_matrix[:, :, i] = dt.predict_proba(X)
+
+        preds = np.sum(preds_matrix, axis=2) / self.n_estimators
+
+        return preds
+
+
+def read_matrix(n, dtype=float):
+    matrix = np.array([list(map(dtype, input().split())) for _ in range(n)])
+    return matrix
+
+
+def read_input_matriсes(n, m, k):
+    X_train, y_train, X_test = read_matrix(n), read_matrix(n), read_matrix(k)
+    return X_train, y_train, X_test
+
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(' '.join(map(str, row)))
+
+
+def solution():
+    n, m, k = map(int, input().split())
+    X_train, y_train, X_test = read_input_matriсes(n, m, k)
+
+    rf = MyRandomForestClassifier(max_features=4, n_estimators=200)
+    rf.fit(X_train, y_train)
+
+    predictions = rf.predict_proba(X_test)
+    print_matrix(predictions)
+
+
+if __name__ == '__main__':
+    solution()
